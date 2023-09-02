9a219359368c5159b0bbbb594edfe753befca655
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-02 09:37:12 +0300

    Create BaseResponse class and StatusCode enum

---
+++ ToDoList.Domain/Enum/StatusCode.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Domain.Enum
+{
+    public enum StatusCode
+    {
+        Ok = 200,
+
+        InvalidServerError = 500,
+    }
+}
---
+++ ToDoList.Domain/Response/BaseResponse.cs
@@ -0,0 +1,22 @@
+﻿using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.Response
+{
+    public class BaseResponse<Type> : IBaseResponse<Type>
+    {
+        public string Description { get; set; }
+
+        public StatusCode StatusCode { get; set; }
+
+        public Type Data { get; set; }
+    }
+
+    public interface IBaseResponse<Type>
+    {
+        string Description { get; set; }
+
+        StatusCode StatusCode { get; set; }
+
+        Type Data { get; set; }
+    }
+}
