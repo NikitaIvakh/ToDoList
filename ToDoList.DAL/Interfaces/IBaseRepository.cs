de1af8cd9b33b67a0bc00842d57e00c42142423f
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-02 09:13:35 +0300

    Create TaskRepository class

---
+++ ToDoList.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoList.DAL.Interfaces
+{
+    public interface IBaseRepository<Type>
+    {
+        Task CreateAsync(Type entity);
+
+        IQueryable<Type> GetAllElements();
+
+        Task<Type> UpdateAsync(Type entity);
+
+        Task DeleteAsync(Type entity);
+    }
+}
---
+++ ToDoList.DAL/Repositories/TaskRepository.cs
@@ -0,0 +1,40 @@
+﻿using ToDoList.DAL.Interfaces;
+using ToDoList.Domain.Entity;
+
+namespace ToDoList.DAL.Repositories
+{
+    public class TaskRepository : IBaseRepository<TaskEntity>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public TaskRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task CreateAsync(TaskEntity entity)
+        {
+            await _applicationDbContext.TaskEntities.AddAsync(entity);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<TaskEntity> GetAllElements()
+        {
+            return _applicationDbContext.TaskEntities;
+        }
+
+        public async Task<TaskEntity> UpdateAsync(TaskEntity entity)
+        {
+            _applicationDbContext.TaskEntities.Update(entity);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(TaskEntity entity)
+        {
+            _applicationDbContext.TaskEntities.Remove(entity);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
