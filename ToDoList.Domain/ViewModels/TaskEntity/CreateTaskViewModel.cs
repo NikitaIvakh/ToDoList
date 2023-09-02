d9d9ce612ec77a187a63e96e6306a7b6dfef0aba
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-02 09:28:27 +0300

    Create CreateTaskViewModel class

---
+++ ToDoList.Domain/ViewModels/TaskEntity/CreateTaskViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.ViewModels.TaskEntity
+{
+    public class CreateTaskViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Priority Priority {  get; set; } 
+    }
+}
