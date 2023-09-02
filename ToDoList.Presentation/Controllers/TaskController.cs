c94f73c57db115fb7ccd4823272f225ed4a8720d
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-02 18:02:02 +0300

    Update TaskController class

--- ToDoList.Presentation/Controllers/TaskController.cs
+++ ToDoList.Presentation/Controllers/TaskController.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.ViewModels.TaskEntity;
+using ToDoList.Service.Interfaces;
 
 namespace ToDoList.Presentation.Controllers
 {
     public class TaskController : Controller
     {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTaskViewModel createTaskViewModel)
+        {
+            var response = await _taskService.CreateTaskAsync(createTaskViewModel);
+            if (response.StatusCode == Domain.Enum.StatusCode.Ok)
+                return Ok(new { description = response.Description });
+
+            return BadRequest(new { description = response.Description });
         }
     }
 }
