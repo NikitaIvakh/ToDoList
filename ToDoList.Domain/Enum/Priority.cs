43c1c6636c98863cc9d20c660cf67aefa43ff7f6
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-02 07:54:45 +0300

    Creaet TaskEntity class with Priority enum

---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.Enum
+{
+    public enum Priority
+    {
+        [Display(Name = "Easy")]
+        Easy = 1,
+
+        [Display(Name = "Medium")]
+        Medium = 2,
+
+        [Display(Name = "Hight")]
+        Hight = 3,
+    }
+}
