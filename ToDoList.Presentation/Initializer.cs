426e3c48d7f281d9b427c5857c7ae16e4985703c
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-02 09:18:25 +0300

    Registering a TaskRepository class

--- ToDoList.Presentation/Initializer.cs
+++ ToDoList.Presentation/Initializer.cs
@@ -1,10 +1,14 @@
-﻿namespace ToDoList.Presentation
+﻿using ToDoList.DAL.Interfaces;
+using ToDoList.DAL.Repositories;
+using ToDoList.Domain.Entity;
+
+namespace ToDoList.Presentation
 {
     public static class Initializer
     {
         public static void InitializeRepositories(this IServiceCollection services)
         {
-
+            services.AddScoped<IBaseRepository<TaskEntity>, TaskRepository>();
         }
 
         public static void InitializeServices(this IServiceCollection services)
