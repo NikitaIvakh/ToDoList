41b4c14b3c44f6850465be5f34f2e76b4d779012
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-03 09:26:02 +0300

    Create TaskViewModel class

---
+++ ToDoList.Domain/ViewModels/TaskEntity/TaskViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.ViewModels.TaskEntity
+{
+    public class TaskViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Title")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Readiness")]
+        public string IsCompleted { get; set; }
+
+        [Display(Name = "Priority")]
+        public string Priority { get; set; }
+
+        [Display(Name = "Date created")]
+        public string DateCreated { get; set; }
+    }
+}
