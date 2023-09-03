8778b31a1e686b7d863f4c620b2eea70ce267a85
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-03 12:38:12 +0300

    Update _Layout view

---
+++ ToDoList.Presentation/wwwroot/js/showAndHide.js
@@ -0,0 +1,59 @@
+﻿function model() {
+    const model = {
+        name: $('input#Name').val(),
+        model: $('input#Model').val(),
+        price: $('input#Price').val(),
+        description: $('input#Description').val()
+    }
+    return model;
+};
+
+$('#saveBtn').on('click', function () {
+    $.ajax({
+        url: '@Url.Action("Save")',
+        type: 'POST',
+        data: model(),
+    })
+});
+
+var modal = $('#modal');
+
+$(".btn-close").click(function () {
+    modal.modal('hide');
+});
+
+function openModal(id) {
+    $.ajax({
+        type: 'GET',
+        url: '/Car/GetCar',
+        data: { "id": id },
+        success: function (response) {
+            modal.find(".modal-body").html(response);
+            modal.modal('show')
+        },
+        failure: function () {
+            modal.modal('hide')
+        },
+        error: function (response) {
+            alert(response.responseText);
+        }
+    });
+};
+
+const hideCardElement = $('#hideCardsId');
+const showCardElement = $('#showCardsId');
+const cardElement = $('.card');
+
+const interval = 2000;
+
+hideCardElement.click(function () {
+    hideCardElement.hide(interval);
+    showCardElement.show(interval);
+    cardElement.hide(interval);
+});
+
+showCardElement.click(function () {
+    hideCardElement.show(interval);
+    showCardElement.hide(interval);
+    cardElement.show(interval);
+});
