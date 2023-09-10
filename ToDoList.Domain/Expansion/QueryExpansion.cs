c500058cc3837bf90d2412321432edda7f0a0a0c
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-10 10:45:17 +0300

    Update QueryExpansion class

--- ToDoList.Domain/Expansion/QueryExpansion.cs
+++ ToDoList.Domain/Expansion/QueryExpansion.cs
@@ -6,7 +6,7 @@ namespace ToDoList.Domain.Expansion
     {
         public static IQueryable<Type> WhereIf<Type>(this IQueryable<Type> source, bool condition, Expression<Func<Type, bool>> predicate)
         {
-            if(condition)
+            if (condition)
                 return source.Where(predicate);
 
             return source;
