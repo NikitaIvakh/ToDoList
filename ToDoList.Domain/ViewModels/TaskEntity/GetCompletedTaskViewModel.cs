4f7f6c1d14f27655cfa834d4aaf8fee69d1e1497
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-10 18:42:53 +0300

    Create GetCompletedTasksViewModel class

---
+++ ToDoList.Domain/ViewModels/TaskEntity/GetCompletedTaskViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.ViewModels.TaskEntity
+{
+    public class GetCompletedTasksViewModel
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Priority { get; set; }
+    }
+}
