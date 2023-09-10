11eb2dbb02fe2efd4570318cef75ae5e87bf4952
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-10 19:26:58 +0300

    Create CsvBaseService class

---
+++ ToDoList.Domain/Helpers/CsvBaseService.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections;
+using System.Globalization;
+using System.Text;
+
+namespace ToDoList.Domain.Helpers
+{
+    public class CsvBaseService<Type>
+    {
+        private readonly CsvConfiguration _csvConfiguration;
+
+        public CsvBaseService()
+        {
+            _csvConfiguration = GetConfiguration();
+        }
+
+        public byte[] UploadFile(IEnumerable data)
+        {
+            using var memoryStream = new MemoryStream();
+            using var streamWriter = new StreamWriter(memoryStream);
+            using var csvWriter = new CsvWriter(streamWriter, _csvConfiguration);
+
+            csvWriter.WriteRecord(data);
+            streamWriter.Flush();
+
+            return memoryStream.ToArray();
+        }
+
+        private static CsvConfiguration GetConfiguration()
+        {
+            return new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";",
+                Encoding = Encoding.UTF8,
+                NewLine = "\r\n",
+            };
+        }
+    }
+}
