8b5c9b8efea5038eb93a1e1a4febdef4224034a7
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-11 08:47:26 +0300

    Create IDataTableResult interface

--- ToDoList.Domain/Response/DataTableResult.cs
+++ ToDoList.Domain/Response/DataTableResult.cs
@@ -1,9 +1,16 @@
 ﻿namespace ToDoList.Domain.Response
 {
-    public class DataTableResult
+    public class DataTableResult : IDataTableResult
     {
         public object Data { get; set; }
 
         public int Total { get; set; }
+    }
+
+    public interface IDataTableResult
+    {
+        object Data { get; }
+
+        int Total { get; }
     }
 }
