5f93244f61c3f6250167e3b546c7c464e42097b7
Nikita Ivakh <maffy145rus@mail.ru>
2023-09-11 08:48:42 +0300

    Add the StatusCode to the DataTableResult class

--- ToDoList.Domain/Response/DataTableResult.cs
+++ ToDoList.Domain/Response/DataTableResult.cs
@@ -1,8 +1,12 @@
-﻿namespace ToDoList.Domain.Response
+﻿using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.Response
 {
     public class DataTableResult : IDataTableResult
     {
         public object Data { get; set; }
+
+        public StatusCode StatusCode { get; set; }
 
         public int Total { get; set; }
     }
@@ -10,6 +14,8 @@
     public interface IDataTableResult
     {
         object Data { get; }
+
+        StatusCode StatusCode { get; }
 
         int Total { get; }
     }
